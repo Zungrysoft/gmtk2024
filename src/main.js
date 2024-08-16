19ca6da8a8aa64104485ed979b19a6ea1981f258
groverburger <19754251+groverburger@users.noreply.github.com>
2024-08-16 12:10:52 -0700

    Basic platformer controls

--- src/main.js
+++ src/main.js
@@ -17,11 +17,62 @@ class Test extends Thing {
   animations = {
     idle: { frames: [0], speed: 0, frameSize: 256 }
   }
-  velocity = [0.1, 0.1]
   position = [game.getWidth() / 2, game.getHeight() / 2]
 
   update () {
+    // Using the default move() method in Thing which handles movement
+    // and collision detection as defined by the checkCollision()
+    // method
     super.update()
+
+    const onGround = this.contactDirections.down
+    const friction = 0.9
+    const groundAcceleration = 2
+    const airAcceleration = 1
+    const acceleration = onGround ? groundAcceleration : airAcceleration
+    
+    // Calculate what the top speed of the player is on the ground,
+    // where friction naturally cancels out any additional
+    // acceleration. This is used to cap max player speed in the air
+    const maxSpeed = groundAcceleration * friction / (1 - friction)
+
+    // Apply gravity
+    this.velocity[1] += 1
+
+    // Friction
+    if (onGround) {
+      this.velocity[0] *= friction
+    }
+
+    // Jump when on ground, and cancel your jump early if you release
+    // space
+    if (game.keysDown.Space && onGround) {
+      this.velocity[1] = -20
+    }
+    if (!game.keysDown.Space && this.velocity[1] < 0) {
+      this.velocity[1] *= 0.7
+    }
+
+    // Move left and right, on ground speed is naturally clamped by
+    // friction but in the air we have to artificially clamp it
+    if (game.keysDown.ArrowRight) {
+      this.velocity[0] += acceleration
+      if (!onGround) {
+        this.velocity[0] = Math.min(this.velocity[0], maxSpeed)
+      }
+    }
+    if (game.keysDown.ArrowLeft) {
+      this.velocity[0] -= acceleration
+      if (!onGround) {
+        this.velocity[0] = Math.max(this.velocity[0], -maxSpeed)
+      }
+    }
+  }
+
+  checkCollision (x, y, z) {
+    // Overloaded collision check to also consider the bottom of the
+    // screen solid
+    return super.checkCollision(x, y, z) || y > game.getHeight() - 64
   }
 }
 
