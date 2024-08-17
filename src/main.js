c49c8b872b0394a9e503d92b7900d30445f76c3b
groverburger <19754251+groverburger@users.noreply.github.com>
2024-08-16 18:29:23 -0700

    Improved player controls and sprite

--- src/main.js
+++ src/main.js
@@ -9,7 +9,7 @@ game.setHeight(720)
 game.createCanvas2D()
 
 game.assets.images = await game.loadImages({
-  guy: 'images/guy.png'
+  guy: 'images/guy3.png'
 })
 
 game.assets.levels = await game.loadText({
@@ -64,33 +64,58 @@ class Level extends Thing {
   }
 }
 
-class Test extends Thing {
+class Player extends Thing {
   sprite = game.assets.images.guy
   animations = {
-    idle: { frames: [0], speed: 0, frameSize: 96 }
+    idle: {
+      frames: [0],
+      speed: 0,
+      frameSize: 96
+    }
   }
   scale = [1 / 48, 1 / 48]
+  jumpBuffer = 0
+  coyoteFrames = 0
+  direction = 1
+  cameraOffset = [0, 0]
+  squash = [1, 1]
 
   update () {
     // Using the default move() method in Thing which handles movement
     // and collision detection as defined by the checkCollision()
     // method
-    //super.update()
     this.updateTimers()
     this.move()
     this.animate()
 
-    // Copy the player's position into the camera by value to prevent
-    // weird double-reference stuff from happening just in case
-    game.getCamera2D().position = [...this.position]
-    game.getCamera2D().scale = [48, 48]
+    // Un-squash and stretch
+    this.squash[0] = u.lerp(this.squash[0], 1, 0.15)
+    this.squash[1] = u.lerp(this.squash[1], 1, 0.15)
+
+    // Camera handling, make sure to offset the camera to look ahead
+    // in the direction the player is currently moving or looking
+    this.cameraOffset[0] = (
+      u.lerp(this.cameraOffset[0], this.direction * 1, 0.05)
+    )
+    this.cameraOffset[1] = u.lerp(
+      this.cameraOffset[1],
+      this.velocity[1],
+      // The camera should move faster when the player is airborne
+      this.velocity[1] === 0 ? 0.05 : 0.1
+    )
+    this.direction = u.sign(this.velocity[0]) || this.direction
+    game.getCamera2D().position = [
+      this.position[0] + this.cameraOffset[0],
+      this.position[1] + this.cameraOffset[1]
+    ]
+    game.getCamera2D().scale = [48, 48] // Make one world unit one tile
 
     const onGround = this.contactDirections.down
-    const friction = 0.9
-    const groundAcceleration = 2 / 48
-    const airAcceleration = 1 / 48
+    const friction = 0.8
+    const groundAcceleration = 2.5 / 48
+    const airAcceleration = 0.5 / 48
     const acceleration = onGround ? groundAcceleration : airAcceleration
-    
+
     // Calculate what the top speed of the player is on the ground,
     // where friction naturally cancels out any additional
     // acceleration. This is used to cap max player speed in the air
@@ -99,15 +124,28 @@ class Test extends Thing {
     // Apply gravity
     this.velocity[1] += 1 / 48
 
-    // Friction
+    // Coyote frames
+    this.coyoteFrames -= 1
     if (onGround) {
-      this.velocity[0] *= friction
+      if (this.coyoteFrames < 5) {
+        this.squash[1] = 0.5
+      }
+      this.coyoteFrames = 6
     }
 
     // Jump when on ground, and cancel your jump early if you release
-    // space
-    if (game.keysDown.Space && onGround) {
-      this.velocity[1] = -20 / 40
+    // space. Buffer times for pressing space too early (jumpBuffer)
+    // and too late (coyoteTime)
+    this.jumpBuffer -= 1
+    if (game.keysPressed.Space) {
+      this.jumpBuffer = 6
+    }
+    if (this.jumpBuffer > 0 && this.coyoteFrames > 0) {
+      this.velocity[1] = -0.41
+      this.jumpBuffer = 0
+      this.coyoteFrames = 0
+      this.squash[1] = 1.5
+      this.squash[0] = 0.5
     }
     if (!game.keysDown.Space && this.velocity[1] < 0) {
       this.velocity[1] *= 0.7
@@ -127,35 +165,22 @@ class Test extends Thing {
         this.velocity[0] = Math.max(this.velocity[0], -maxSpeed)
       }
     }
+
+    // Apply friction
+    if (onGround) {
+      this.velocity[0] *= friction
+    }
+
+    // Apply scaling
+    this.scale[0] = this.direction * this.squash[0] / 48
+    this.scale[1] = this.squash[1] / 48
   }
 
-  move (iterations = 16) {
-    const dx = this.velocity[0] / iterations
-    const dy = this.velocity[1] / iterations
-
-    for (const key in this.contactDirections) {
-      this.contactDirections[key] = false
-    }
-
-    for (let i = 0; i < iterations; i += 1) {
-      if (this.checkCollision(this.position[0] + dx, this.position[1])) {
-        if (u.sign(dx) > 0) { this.contactDirections.right = true }
-        if (u.sign(dx) < 0) { this.contactDirections.left = true }
-        this.velocity[0] = 0
-        break
-      }
-      this.position[0] += dx
-    }
-
-    for (let i = 0; i < iterations; i += 1) {
-      if (this.checkCollision(this.position[0], this.position[1] + dy)) {
-        if (u.sign(dy) > 0) { this.contactDirections.down = true }
-        if (u.sign(dy) < 0) { this.contactDirections.up = true }
-        this.velocity[1] = 0
-        break
-      }
-      this.position[1] += dy
-    }
+  draw () {
+    // Move the sprite down a bit when it's being squashed, so it
+    // looks like it's taking off from the ground when the player
+    // jumps
+    this.drawSprite(...this.position, 0, u.map(this.squash[1], 1, 0.5, 0, 32, true))
   }
 
   checkCollision (x, y, z) {
@@ -165,7 +190,7 @@ class Test extends Thing {
 
     const w = 0.25
     const h = 1
-    const headRoom = 0.1 // A little headroom to make low ceilings easier
+    const headRoom = 0.4 // A little headroom to make low ceilings easier
 
     // Doing a bunch of point collisions around the perimeter of the
     // player's bounding box with the level's tile grid to simulate a
@@ -185,5 +210,5 @@ class Test extends Thing {
 
 game.setScene(() => {
   game.addThing(new Level(game.assets.levels.level1))
-  game.addThing(new Test())
+  game.addThing(new Player())
 })
