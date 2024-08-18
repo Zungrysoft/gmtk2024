0d8894a1efb24f37b9e93b5c9a8c6b1c835f08a1
groverburger <19754251+groverburger@users.noreply.github.com>
2024-08-18 01:20:59 -0700

    Selling and money polish

--- src/sellzone.js
+++ src/sellzone.js
@@ -26,6 +26,7 @@ export default class SellZone extends Thing {
       ) {
         thing.isDead = true
         player.money += thing.price
+        game.addThing(new MoneyPopup(thing.price, [...this.position]))
       }
     }
   }
@@ -37,6 +38,40 @@ export default class SellZone extends Thing {
     ctx.globalAlpha = 0.5
     ctx.translate(...this.position)
     ctx.fillRect(-1, -1, 2, 2)
+    ctx.restore()
+  }
+}
+
+class MoneyPopup extends Thing {
+  depth = 100
+
+  constructor (amount, position) {
+    super()
+    this.position = position
+    this.amount = amount
+    this.velocity[1] = -0.03
+    this.setTimer('life', 80, () => { this.isDead = true })
+  }
+
+  update () {
+    super.update()
+    this.velocity[1] *= 0.9
+  }
+
+  draw () {
+    const { ctx } = game
+    if (this.getTimerFrames('life') < 30 && this.getTimerFrames('life') % 6 < 2) {
+      //return
+    }
+
+    ctx.save()
+    ctx.fillStyle = 'rgb(120, 255, 80)'
+    ctx.globalAlpha = u.inverseSquareMap(this.getTimer('life'), 0.5, 1, 1, 0, true)
+    ctx.textAlign = 'center'
+    ctx.font = 'bold 36px Arial'
+    ctx.translate(...this.position)
+    ctx.scale(1 / 48, 1 / 48)
+    ctx.fillText(`+$${this.amount}`, 0, 0)
     ctx.restore()
   }
 }
